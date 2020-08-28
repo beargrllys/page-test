093e7c17bbc734b2886f1c121e11b26dbab819d9
beargrllys <js_7744@naver.com>
2020-08-28 14:24:28 +0900

    Updates

---
+++ precache-manifest.3c058cef35154874e785565451c436a6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55b3e4bf456ae4d8d73515a96deb1624",
+    "url": "/page-test/index.html"
+  },
+  {
+    "revision": "a77e2767e35f9af1981f",
+    "url": "/page-test/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "9b9d081565d18cd59681",
+    "url": "/page-test/static/css/main.451a770b.chunk.css"
+  },
+  {
+    "revision": "a77e2767e35f9af1981f",
+    "url": "/page-test/static/js/2.eb16042e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/page-test/static/js/2.eb16042e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b9d081565d18cd59681",
+    "url": "/page-test/static/js/main.8cdd1950.chunk.js"
+  },
+  {
+    "revision": "73d19b0443876b299319",
+    "url": "/page-test/static/js/runtime-main.d555d6d0.js"
+  },
+  {
+    "revision": "575d44c74e8bc37b0c9ae28bc509151e",
+    "url": "/page-test/static/media/LOGO.575d44c7.png"
+  },
+  {
+    "revision": "f711c6e9b544bb609baeeaa2a0d26142",
+    "url": "/page-test/static/media/adjust.f711c6e9.png"
+  },
+  {
+    "revision": "67e1276b79d93ba135f5dbfb9405bf51",
+    "url": "/page-test/static/media/profileImage.67e1276b.png"
+  }
+]);
